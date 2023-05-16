3babff78cc0f7ab1fcb4909739ed17e25b2edbdb
Rajpal9 <rajpal942@gmail.com>
2023-05-16 15:16:59 +0530

    Correct Dynamics

---
+++ core/neural_fwd_map.py
@@ -0,0 +1,198 @@
+"""CREATE A NEURAL NETWORK TO MAP FROM THETA TO POSITION OF END EFFECTOR"""
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import random_split
+import torch.optim as optim
+from torch.utils.data import random_split
+import os
+import numpy as np
+from sys import exit
+import matplotlib.pyplot as plt
+
+class map_Net(nn.Module):
+
+    def __init__(self,map_net_params):
+        super(map_Net, self).__init__()
+        self.map_net_params = map_net_params
+        self.opt_parameters_map = []
+
+
+
+    def create_model(self):
+        input_dim = self.map_net_params['input_dim']
+        hidden_depth = self.map_net_params['hidden_depth']
+        hidden_width = self.map_net_params['hidden_width']
+        output_dim = self.map_net_params['output_dim']
+        activation_type = self.map_net_params['activation_type']
+
+
+        if hidden_depth>0:
+            #input_layer
+            self.map_net_in = nn.Linear(input_dim,hidden_width)
+            self.opt_parameters_map.append(self.map_net_in.weight)
+            self.opt_parameters_map.append(self.map_net_in.bias)
+            self.map_net_hid = nn.ModuleList()
+
+            #hidden layers
+            for i in range(1,hidden_depth):
+                self.map_net_hid.append(nn.Linear(hidden_width,hidden_width))
+                self.opt_parameters_map.append(self.map_net_hid[-1].weight)
+                self.opt_parameters_map.append(self.map_net_hid[-1].bias)
+
+
+            self.map_net_out = nn.Linear(hidden_width,output_dim)
+            self.opt_parameters_map.append(self.map_net_out.weight)
+            self.opt_parameters_map.append(self.map_net_out.bias)
+
+
+        else: #no hidden layers
+            self.map_net_out = nn.Linear(input_dim,output_dim)
+            self.opt_parameters_map.append(self.map_net_out.weight)
+            self.opt_parameters_map.append(self.map_net_out.bias)
+
+
+        #activation functions
+        if activation_type == 'relu':
+            self.activation_fcn = F.relu
+
+        elif activation_type == 'elu':
+            self.activation_fcn = nn.ELU
+
+        elif activation_type == 'tanh':
+            self.activation_fcn = torch.tanh
+
+        elif activation_type == 'sigmoid':
+            self.activation_fcn = torch.sigmoid
+
+        else:
+            exit("Exit : invalid activation function")
+
+
+    def forward(self, theta):
+        #this computes the forward operations
+        if self.map_net_params['hidden_depth']>0:
+            x = self.activation_fcn(self.map_net_in(theta))
+
+            for layer in self.map_net_hid:
+                x = self.activation_fcn(layer(x))
+
+        x = self.map_net_out(x)
+
+        return x
+
+
+    def set_optimizer(self):
+        if self.map_net_params['optimizer']=='adam':
+            lr = self.map_net_params['lr']
+            weight_decay = self.map_net_params['l2_reg']
+            self.optimizer = optim.Adam(self.parameters(),lr = lr, weight_decay = weight_decay)
+        elif self.map_net_params['optimizer']=='adamax':
+            lr = self.map_net_params['lr']
+            weight_decay = self.map_net_params['l2_reg']
+            self.optimizer = optim.Adamax(self.parameters(),lr = lr, weight_decay = weight_decay)
+
+
+    def loss(self,output, label):
+        criterion = nn.MSELoss()
+        loss = criterion(output,label)
+        #l1 regularization
+        l1_reg = self.map_net_params['l1_reg']
+        l1_norm = torch.norm(self.map_net_in.weight.view(-1),p=1)+torch.norm(self.map_net_in.bias.view(-1),p=1)+torch.norm(self.map_net_out.weight.view(-1),p=1)+torch.norm(self.map_net_out.bias.view(-1),p=1)
+        for i in range(0,self.map_net_params['hidden_depth']-1):
+            l1_norm+= torch.norm(self.map_net_hid[i].weight.view(-1),p=1)+torch.norm(self.map_net_hid[i].bias.view(-1),p=1)
+
+        l1_loss = l1_reg*l1_norm
+        total_loss = loss+l1_loss
+
+        return total_loss
+
+
+    def set_dataset_map(self,xs_map_train,ys_map_train,xs_map_val,ys_map_val):
+        order = 'F'
+        n = self.map_net_params['input_dim'] #number of angles
+        m = self.map_net_params['output_dim'] #x and y position of end effector
+        n_traj_train = xs_map_train.shape[0]
+        n_traj_val = int(0.25*n_traj_train)
+        n_data_pts_train = n_traj_train*xs_map_train.shape[1]
+        n_data_pts_val = n_traj_val*xs_map_train.shape[1]
+
+        self.xs_map_train = xs_map_train.T.reshape((n,n_data_pts_train),order=order)
+        self.ys_map_train = ys_map_train.T.reshape((m,n_data_pts_train),order=order)
+        self.xs_map_val = xs_map_val.T.reshape((n,n_data_pts_val),order=order)
+        self.ys_map_val = ys_map_val.T.reshape((m,n_data_pts_val),order=order)
+
+        self.xs_map_train= self.xs_map_train.T
+        self.xs_map_val= self.xs_map_val.T
+        self.ys_map_train= self.ys_map_train.T
+        self.ys_map_val= self.ys_map_val.T
+
+    def data_pipeline(self):
+        self.create_model()
+        self.set_optimizer()
+        X_map_train_t, y_map_train_t = torch.from_numpy(self.xs_map_train).float(), torch.from_numpy(self.ys_map_train).float()
+        X_map_val_t, y_map_val_t = torch.from_numpy(self.xs_map_val).float(), torch.from_numpy(self.ys_map_val).float()
+        dataset_train = torch.utils.data.TensorDataset(X_map_train_t, y_map_train_t)
+        dataset_val = torch.utils.data.TensorDataset(X_map_val_t, y_map_val_t)
+
+        self.train_map_model(dataset_train,dataset_val)
+
+
+    def train_map_model(self,dataset_train,dataset_val):
+        # Train Network
+        trainloader = torch.utils.data.DataLoader(dataset_train, batch_size = self.map_net_params['batch_size'], shuffle = True)
+        valloader = torch.utils.data.DataLoader(dataset_val, batch_size = self.map_net_params['batch_size'])
+
+        val_loss_prev = np.inf
+        self.train_loss_hist = []
+        self.val_loss_hist = []
+        for epoch in range(self.map_net_params['epochs']):
+            running_loss = 0.0
+            epoch_steps = 0
+
+            for data in trainloader:
+                inputs, labels = data
+
+                self.optimizer.zero_grad()
+                output = self(inputs)
+                loss = self.loss(output,labels)
+                loss.backward()
+                self.optimizer.step()
+
+                running_loss += float(loss.detach())
+                epoch_steps += 1
+
+            val_loss = 0.0
+            val_steps =0
+
+            for data in valloader:
+                with torch.no_grad():
+                    inputs, val_labels = data
+
+                    val_output = self(inputs)
+                    loss= self.loss(val_output,val_labels)
+
+                    val_loss += float(loss.detach())
+                    val_steps += 1
+
+            #print epoch loss
+
+            self.train_loss_hist.append(running_loss/epoch_steps)
+            self.val_loss_hist.append(val_loss/val_steps)
+            print('Epoch %3d: train loss: %.10f, validation loss: %.10f' %(epoch + 1, self.train_loss_hist[-1],self.val_loss_hist[-1]))
+
+        print("Finished training")
+
+    def plot_loss(self):
+        #plot
+        epochs = np.arange(0, self.map_net_params['epochs'])
+        plt.figure(figsize=(15,8))
+        plt.plot(epochs, self.train_loss_hist, color='tab:orange', label='Training loss')
+        plt.plot(epochs, self.val_loss_hist, color='tab:blue', label='Validation loss')
+
+        plt.legend()
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.yscale('log')
+        plt.show()
---
+++ dynamics/.ipynb_checkpoints/robot_3R-checkpoint.py
@@ -0,0 +1,212 @@
+import numpy as np
+
+"Kinematics of 3r"
+def forward_kinematics_3R(th_data,th_dot_data, robot_pars):
+    #define parameters
+    # link lengths
+    l1 = robot_pars['l1']
+    l2 = robot_pars['l2']
+    #link masses
+    m1 = robot_pars['m1']
+    m2 = robot_pars['m2']
+    # link inertias
+    I1 = robot_pars['I1']
+    I2 = robot_pars['I1']
+    # theta_data = (num_snaps, num_states)
+    
+    # X  = rows are joint and effector positions and velocities
+    num_states = 4*th_data.shape[1]
+    num_snaps = th_data.shape[0]
+    X = np.empty((num_snaps,num_states)) # cartesian state matrix
+
+    for i in range(num_snaps):
+        # time evolution
+        X[i,0] = l1*np.cos(th_data[i,0]) + l2*np.cos(th_data[i,0]+th_data[i,1]) # end_eff_x
+        X[i,1] = l1*np.sin(th_data[i,0]) + l2*np.sin(th_data[i,0]+th_data[i,1]) # end_eff_y
+        X[i,2] = l1*np.cos(th_data[i,0]) #jt_x
+        X[i,3] = l1*np.sin(th_data[i,0]) #jt_y
+        X[i,4] = -l1*np.sin(th_data[i,0])*th_dot_data[i,0] - l2*np.sin(th_data[i,0]+th_data[i,1])*(th_dot_data[i,0]+th_dot_data[i,1]) # end_eff_x
+        X[i,5] = l1*np.cos(th_data[i,0])*th_dot_data[i,0] + l2*np.cos(th_data[i,0]+th_data[i,1])*(th_dot_data[i,0]+th_dot_data[i,1]) # end_eff_y
+        X[i,6] = -l1*np.sin(th_data[i,0])*th_dot_data[i,0] #jt_x
+        X[i,7] = l1*np.cos(th_data[i,0])*th_dot_data[i,0] #jt_y
+    return X
+
+"forward map of 3R"
+def forward_map_3R(th_data, robot_pars):
+    #define parameters
+    # link lengths
+    l1 = robot_pars['l1']
+    l2 = robot_pars['l2']
+    #link masses
+    m1 = robot_pars['m1']
+    m2 = robot_pars['m2']
+    # link inertias
+    I1 = robot_pars['I1']
+    I2 = robot_pars['I1']
+    # theta_data = (num_snaps, num_states)
+    
+    # X  = rows are joint and effector positions and velocities
+    num_states = 4
+    num_snaps = 1
+    X = np.empty((num_states,)) # cartesian state matrix
+
+    # time evolution
+    X[0] = l1*np.cos(th_data[0]) + l2*np.cos(th_data[0]+th_data[1]) # end_eff_x
+    X[1] = l1*np.sin(th_data[0]) + l2*np.sin(th_data[0]+th_data[1]) # end_eff_y
+    X[2] = l1*np.cos(th_data[0]) #jt_x
+    X[3] = l1*np.sin(th_data[0]) #jt_y  
+    
+    return X
+
+"Jacobian of 3R"
+def Jacobian_3R(th_data, robot_pars):
+    #define parameters
+    # link lengths
+    l1 = robot_pars['l1']
+    l2 = robot_pars['l2']
+    #link masses
+    m1 = robot_pars['m1']
+    m2 = robot_pars['m2']
+    # link inertias
+    I1 = robot_pars['I1']
+    I2 = robot_pars['I1']
+    # theta_data = (num_snaps, num_states)
+    
+    # X  = rows are joint and effector positions and velocities
+    J = np.empty((2,2)) # cartesian state matrix
+
+    # time evolution
+    J[0,0] = -l1*np.sin(th_data[0]) - l2*np.sin(th_data[0]+th_data[1]) # end_eff_x
+    J[0,1] = -l2*np.sin(th_data[0]+th_data[1])
+    J[1,0] = l1*np.cos(th_data[0]) + l2*np.cos(th_data[0]+th_data[1])
+    J[1,1] = l2*np.cos(th_data[0]+th_data[1])
+    
+    return J
+
+"""Dynamics of 2R"""
+def dynamics_3R(dt, u, th, th_dot, robot_pars):
+    #define parameters
+    # link lengths
+
+    # link lengths
+    a1 = robot_pars['a1']
+    a2 = robot_pars['a2']
+    a3 = robot_pars['a3']
+
+    #link masses
+    m1 = robot_pars['m1']
+    m2 = robot_pars['m2']
+    m3 = robot_pars['m3']
+
+    # link inertias
+    I1 = robot_pars['I1']
+    I2 = robot_pars['I2']
+    I3 = robot_pars['I3']
+
+    #position of CM
+    l1 = robot_pars['l1']
+    l2 = robot_pars['l2']
+    l3 = robot_pars['l3']
+
+    #dynamic friction coefficient
+    fd = robot_pars['fd']
+
+    # gravity
+    g = 9.81*0
+    # matrix initializations
+    
+    ##
+    #define thetas and theta_dots for convenience
+    th1 = th[0]
+    th2 = th[1]
+    th3 = th[2]
+    th1_dot = th_dot[0]
+    th2_dot = th_dot[1]
+    th3_dot = th_dot[2]
+
+    
+    tau = u.reshape(-1,1)
+
+    # mass matrix
+    m11 = I1+I2+I3+m1*l1**2+m2*(a1**2+l2**2+2*a1*l2*np.cos(th2))+m3*(a1**2+a2**2+l3**2+2*a1*a2*np.cos(th2)+2*a1*l3*np.cos(th2+th3)+2*a2*l3*np.cos(th3))
+    m22 = I2+I3+m2*l2**2+m3*(a2**2+l3**2+2*a2*l3*np.cos(th3))
+    m33 = I3+m3*l3**2
+    m12 = I2+I3+m2*(l2**2+a1*l2*np.cos(th2))+m3*(a2**2+l3**2+a1*a2*np.cos(th2)+a1*l3*np.cos(th2+th3)+2*a2*l3*np.cos(th3))
+    m21 = m12
+    m13 = I3+m3*(l3**2+a1*l3*np.cos(th2+th3)+a2*l3*np.cos(th3))
+    m31 = m13
+    m23 = I3+m3*(l3**2+a2*l3*np.cos(th3))
+    m32 = m23
+
+
+    M = np.array([[m11,m12,m13],[m21,m22,m23],[m31,m32,m33]])
+              
+
+    # coriolis terms
+    c11 = -(m3*a1*l3*np.sin(th2+th3)+m3*a2*l3*np.sin(th3))*th3_dot-(m2+m3*a1*a2*np.sin(th2)+a1*l2*np.sin(th2)+m3*a1*l3*np.sin(th2+th3))*th2_dot
+    c22 = -m3*a2*l3*np.sin(th3)*th3_dot
+    c33 = -(m3*a1*a2*np.sin(th2))*th2_dot-(m3*a2*l3*np.sin(th3))*th3_dot
+    c12 = -(m2*a1*l2*np.sin(th2)+m3*a1*l3*np.sin(th2+th3)+m3*a1*a2*np.sin(th2))*th1_dot-(m3*a1*a2*np.sin(th2)+m3*a1*l3*np.sin(th2+th3)+m2*a1*l2*np.sin(th2))*th2_dot-(m3*a2*l3*np.sin(th3)+m3*a1*l3*np.sin(th2+th3))*th3_dot
+    c13 = -(m3*a1*l3*np.sin(th2+th3)+m3*a2*l3*np.sin(th3))*th1_dot-(m3*a1*l3*np.sin(th2+th3)+m3*a2*l3*np.sin(th3))*th2_dot-(m3*a2*l3*np.sin(th2)+m3*a1*l3*np.sin(th2+th3))*th3_dot
+    c21 = (m2*a1*l2*np.sin(th2)+m3*a1*a2*np.sin(th2)+m3*a1*l3*np.sin(th2+th3))*th1_dot-(m3*a2*l3*np.sin(th3))*th3_dot
+    c23 = -(m3*a2*l3*np.sin(th3))*th1_dot-(m3*a2*l3*np.sin(th3))*th2_dot-(m3*a2*l3*np.sin(th3))*th3_dot
+    c31 = (m3*a1*l3*np.sin(th2+th3)+m3*a2*l3*np.sin(th3))*th1_dot+(m3*a2*l3*np.sin(th3))*th2_dot
+    c32 = (m3*a2*l3*np.sin(th3))*th1_dot+(m3*a2*l3*np.sin(th3))*th2_dot-(m3*a2*l3*np.sin(th3))*th3_dot
+
+    C = np.array([[c11,c12,c13],[c21,c22,c23],[c31,c32,c33]])
+
+
+    # gravity terms
+    g1 = (m2*l2+m3*a2)*g*np.cos(th1+th2)+m3*l3*g*np.cos(th1+th2+th3)+(m1*l1+m3*a1+m2*a1)*g*np.cos(th1)
+    g2 = (m2*l2+m3*a2)*g*np.cos(th1+th2)+m3*l3*g*np.cos(th1+th2+th3)
+    g3 = m3*l3*g*np.cos(th1+th2+th3)
+
+
+    G = np.array([[g1],[g2],[g3]])
+    
+
+    
+    # theta evolution
+    th_ddot = np.matmul(np.linalg.inv(M),(tau-G-np.matmul(C,th_dot).reshape(-1,1)-(fd*th_dot).reshape(-1,1)).reshape(-1,))
+    th_dot_next = th_dot + th_ddot*dt
+    th_next = th + th_dot*dt + (1/2)*th_ddot*dt**2
+
+    return th_next, th_dot_next
+
+
+"""Data Generation for training"""
+
+def dynamics_3R_data_gen(dt,num_traj,num_snaps,num_states,num_inputs, robot_pars):
+    
+    # link lengths
+    a1 = robot_pars['a1']
+    a2 = robot_pars['a2']
+    a3 = robot_pars['a3']
+
+    # matrix initializations
+    X = np.empty((num_traj,num_snaps+1,num_states)) # cartesian state matrix
+    U = np.empty((num_traj,num_snaps,num_inputs)) # input matrix
+    x_end = np.empty((num_traj,num_snaps+1,2)) # position of end effector
+
+    for i in range(num_traj):
+    # initialize the values for the trajectory
+      # joint angles
+        X[i,0,0:3] = np.pi*(2*np.random.rand(1,1,3)-1) # theta values
+          # joint velocities
+        X[i,0,3:6] = 0.1*(2*np.random.rand(1,1,3)-1) # theta values
+        x_end[i,0,0]= a1*np.cos(X[i,0,0])+a2*np.cos(X[i,0,0]+X[i,0,1])+a3*np.cos(X[i,0,0]+X[i,0,1]+X[i,0,2])
+        x_end[i,0,1]= a1*np.sin(X[i,0,0])+a2*np.sin(X[i,0,0]+X[i,0,1])+a3*np.sin(X[i,0,0]+X[i,0,1]+X[i,0,2])
+
+
+        for j in range(num_snaps):
+
+            # inputs
+            U[i,j,:] = 1*(2*np.random.rand(1,1,3)-1)
+
+            # theta evolution
+            X[i,j+1,0:3], X[i,j+1,3:6] = dynamics_3R(dt, U[i,j,:], X[i,j,0:3],  X[i,j,3:6], robot_pars)
+
+            x_end[i,j+1,0]= a1*np.cos(X[i,j+1,0])+a2*np.cos(X[i,j+1,0]+X[i,j+1,1])+a3*np.cos(X[i,j+1,0]+X[i,j+1,1]+X[i,j+1,2])
+            x_end[i,j+1,1]= a1*np.sin(X[i,j+1,0])+a2*np.sin(X[i,j+1,0]+X[i,j+1,1])+a3*np.sin(X[i,j+1,0]+X[i,j+1,1]+X[i,j+1,2])
+
+    return x_end,X,U
