ad8e8bb679f23d5a6eaae0f1208083b2be51e4b0
rajpal9 <rajpla942@gmail.com>
2023-07-28 14:49:33 +0530

    Paths and Computation Time added

--- paths/path_2D.m
+++ paths/path_2D.m
@@ -11,56 +11,47 @@ function [rd, rd_dot, rd_ddot] = path_2D(c,tilt, rd_0,t, t_end, shape)
 % rd_dot -  desired velocity
 % rd_ddot -  desired position
     t = t*10/t_end;
+    n = 10/t_end;
     if strcmpi(shape,'adobe') == 1
         rd = [2*c*cos(0.4*pi*t) + 3*c*cos(0.2*pi*t) + rd_0(1) - 5*c;
               2*c*sin(0.4*pi*t) - 3*c*sin(0.2*pi*t) + rd_0(2)];
            
-        rd_dot = [-0.8*pi*c*sin(0.4*pi*t) - 0.6*pi*c*sin(0.2*pi*t);
-                   0.8*pi*c*cos(0.4*pi*t) - 0.6*pi*c*cos(0.2*pi*t)];
+        rd_dot = [-0.8*(pi*n)*c*sin(0.4*pi*t) - 0.6*(pi*n)*c*sin(0.2*pi*t);
+                   0.8*(pi*n)*c*cos(0.4*pi*t) - 0.6*(pi*n)*c*cos(0.2*pi*t)];
                    
-        rd_ddot = [-0.32*pi^2*c*cos(0.4*pi*t) - 0.12*pi^2*c*cos(0.2*pi*t);
-                  -0.32*pi^2*c*sin(0.4*pi*t) + 0.12*pi^2*c*sin(0.2*pi*t)];
+        rd_ddot = [-0.32*(pi*n)^2*c*cos(0.4*pi*t) - 0.12*(pi*n)^2*c*cos(0.2*pi*t);
+                  -0.32*(pi*n)^2*c*sin(0.4*pi*t) + 0.12*(pi*n)^2*c*sin(0.2*pi*t)];
               
-    elseif strcmpi(shape,'cool_2') == 1
+    elseif strcmpi(shape,'hypotrochoid') == 1
        rd = [2*c*cos(0.2*pi*t) - c*cos(0.8*pi*t) + rd_0(1) - c;
                        2*c*sin(0.2*pi*t) - c*sin(0.8*pi*t) + rd_0(2)];
                    
-        rd_dot =  [-0.4*c*pi*sin(0.2*pi*t)+ 0.8*c*pi*sin(0.8*pi*t);
-                   0.4*c*pi*cos(0.2*pi*t)- 0.8*c*pi*cos(0.8*pi*t)];
+        rd_dot =  [-0.4*c*(pi*n)*sin(0.2*pi*t)+ 0.8*c*(pi*n)*sin(0.8*pi*t);
+                   0.4*c*(pi*n)*cos(0.2*pi*t)- 0.8*c*(pi*n)*cos(0.8*pi*t)];
                    
-        rd_ddot = [-0.08*c*pi^2*cos(0.2*pi*t)+ 0.64*c*pi^2*cos(0.8*pi*t);
-                 -0.08*c*pi^2*sin(0.2*pi*t) + 0.64*c*pi^2*sin(0.8*pi*t)];
+        rd_ddot = [-0.08*c*(pi*n)^2*cos(0.2*pi*t)+ 0.64*c*(pi*n)^2*cos(0.8*pi*t);
+                 -0.08*c*(pi*n)^2*sin(0.2*pi*t) + 0.64*c*(pi*n)^2*sin(0.8*pi*t)];
              
-    elseif strcmpi(shape,'cool_3') == 1
-       rd = [c*cos(0.2*pi*t) + 2*c*cos(0.6*pi*t) + rd_0(1) - 3*c;
-             c*sin(0.2*pi*t) - 2*c*sin(0.8*pi*t) + rd_0(2)];
-                   
-        rd_dot =  [-0.2*c*pi*sin(0.2*pi*t) - 0.8*c*pi*sin(0.8*pi*t);
-                   0.2*c*pi*cos(0.2*pi*t)- 0.8*c*pi*cos(1.2*pi*t)];
-                   
-        rd_ddot = [-0.04*c*pi^2*cos(0.2*pi*t)- 0.64*c*pi^2*cos(0.8*pi*t);
-                 -0.04*c*pi^2*sin(0.2*pi*t) + 0.64*c*pi^2*sin(0.8*pi*t)];
-        
     
     elseif strcmpi(shape,'circle') == 1
        rd =  [c*cos(0.2*pi*t)+ rd_0(1) - c;
               c*sin(0.2*pi*t) + rd_0(2)];
                    
-       rd_dot = [-c*0.2*pi*sin(0.2*pi*t);
-                  c*0.2*pi*cos(0.2*pi*t)];
+       rd_dot = [-c*0.2*(pi*n)*sin(0.2*pi*t);
+                  c*0.2*(pi*n)*cos(0.2*pi*t)];
                    
-       rd_ddot = [-c*((0.2*pi)^2)*cos(0.2*pi*t);
-                  -c*((0.2*pi)^2)*sin(0.2*pi*t)];
+       rd_ddot = [-c*((0.2*pi*n)^2)*cos(0.2*pi*t);
+                  -c*((0.2*pi*n)^2)*sin(0.2*pi*t)];
     
     elseif strcmpi(shape,'cardioid') == 1
        rd = [2*c*cos(0.2*pi*t)-c*cos(0.4*pi*t)+rd_0(1)-c;
              2*c*sin(0.2*pi*t)-c*sin(0.4*pi*t)+rd_0(2)];
                    
-       rd_dot = [-2*c*0.2*pi*sin(0.2*pi*t)+c*0.4*pi*sin(0.4*pi*t);
-                  2*c*0.2*pi*cos(0.2*pi*t)-c*0.4*pi*cos(0.4*pi*t)];
+       rd_dot = [-2*c*0.2*(pi*n)*sin(0.2*pi*t)+c*0.4*(pi*n)*sin(0.4*pi*t);
+                  2*c*0.2*(pi*n)*cos(0.2*pi*t)-c*0.4*(pi*n)*cos(0.4*pi*t)];
                    
-        rd_ddot = [-2*c*0.2^2*pi^2*cos(0.2*pi*t)+c*0.4^2*pi^2*cos(0.4*pi*t);
-                   -2*c*(0.2*pi)^2*sin(0.2*pi*t)+c*0.4^2*pi^2*sin(0.4*pi*t)];
+        rd_ddot = [-2*c*0.2^2*(pi*n)^2*cos(0.2*pi*t)+c*0.4^2*(pi*n)^2*cos(0.4*pi*t);
+                   -2*c*(0.2*pi*n)^2*sin(0.2*pi*t)+c*0.4^2*(pi*n)^2*sin(0.4*pi*t)];
              
              
     
@@ -68,32 +59,53 @@ function [rd, rd_dot, rd_ddot] = path_2D(c,tilt, rd_0,t, t_end, shape)
        rd = [c*cos(0.4*pi*t)+2*c*cos(0.2*pi*t)+ rd_0(1)-3*c;
              c*sin(0.4*pi*t)-2*c*sin(0.2*pi*t)+rd_0(2)];
                    
-        rd_dot = [-c*0.4*pi*sin(0.4*pi*t)-2*c*0.2*pi*sin(0.2*pi*t);
-                   c*0.4*pi*cos(0.4*pi*t)-2*c*0.2*pi*cos(0.2*pi*t)];
+        rd_dot = [-c*0.4*(pi*n)*sin(0.4*pi*t)-2*c*0.2*(pi*n)*sin(0.2*pi*t);
+                   c*0.4*(pi*n)*cos(0.4*pi*t)-2*c*0.2*(pi*n)*cos(0.2*pi*t)];
                    
-        rd_ddot = [-c*((0.4*pi)^2)*cos(0.4*pi*t)-2*c*((0.2*pi)^2)*cos(0.2*pi*t);
-                   -c*((0.4*pi)^2)*sin(0.4*pi*t)+2*c*((0.2*pi)^2)*sin(0.2*pi*t)];
+        rd_ddot = [-c*((0.4*pi*n)^2)*cos(0.4*pi*t)-2*c*((0.2*pi*n)^2)*cos(0.2*pi*t);
+                   -c*((0.4*pi*n)^2)*sin(0.4*pi*t)+2*c*((0.2*pi*n)^2)*sin(0.2*pi*t)];
     
     elseif strcmpi(shape,'star') == 1
        rd = [c*cos(0.6*pi*t)+2*c*cos(0.4*pi*t)+rd_0(1)-3*c;
              c*sin(0.6*pi*t)-2*c*sin(0.4*pi*t)+rd_0(2)];
                    
-        rd_dot = [-c*0.6*pi*sin(0.6*pi*t)-2*c*0.4*pi*sin(0.4*pi*t);
-                 c*0.6*pi*cos(0.6*pi*t)-2*c*0.4*pi*cos(0.4*pi*t)];
+        rd_dot = [-c*0.6*(pi*n)*sin(0.6*pi*t)-2*c*0.4*(pi*n)*sin(0.4*pi*t);
+                 c*0.6*(pi*n)*cos(0.6*pi*t)-2*c*0.4*(pi*n)*cos(0.4*pi*t)];
                    
-        rd_ddot =  [-c*((0.6*pi)^2)*cos(0.6*pi*t)-2*c*((0.4*pi)^2)*cos(0.4*pi*t);
-                    -c*((0.6*pi)^2)*sin(0.6*pi*t)+2*c*((0.4*pi)^2)*sin(0.4*pi*t)];
+        rd_ddot =  [-c*((0.6*pi*n)^2)*cos(0.6*pi*t)-2*c*((0.4*pi*n)^2)*cos(0.4*pi*t);
+                    -c*((0.6*pi*n)^2)*sin(0.6*pi*t)+2*c*((0.4*pi*n)^2)*sin(0.4*pi*t)];
                 
-    elseif strcmpi(shape,'petal') == 1
-       rd = [c*cos(0.2*pi*t) + c*cos(1.2*pi*t)+ rd_0(1)- 2*c;
-             c*sin(0.2*pi*t)- c*sin(1.2*pi*t)+rd_0(2)];  
+    elseif strcmp(shape,'petal')==1
+       rd = [c*cos(0.2*pi*t) + c*cos(0.6*pi*t)+ rd_0(1)- 2*c;
+             c*sin(0.2*pi*t)- c*sin(0.6*pi*t)+rd_0(2)];  
        
-       rd_dot = [-(0.2*pi)*c*sin(0.2*pi*t) - (0.6*pi)*c*sin(0.6*pi*t);
-                 (0.2*pi)*c*cos(0.2*pi*t) - (0.6*pi)*c*cos(0.6*pi*t)];  
+       rd_dot = [-(0.2*pi*n)*c*sin(0.2*pi*t) - (0.6*pi*n)*c*sin(0.6*pi*t);
+                 (0.2*pi*n)*c*cos(0.2*pi*t) - (0.6*pi*n)*c*cos(0.6*pi*t)];  
        
-       rd_ddot = [-((0.2*pi)^2)*c*cos(0.2*pi*t) - ((0.6*pi)^2)*c*cos(0.6*pi*t);
-                 -((0.2*pi)^2)*c*sin(0.2*pi*t) + ((0.6*pi)^2)*c*sin(0.6*pi*t)];
+       rd_ddot = [-((0.2*pi*n)^2)*c*cos(0.2*pi*t) - ((0.6*pi*n)^2)*c*cos(0.6*pi*t);
+                 -((0.2*pi*n)^2)*c*sin(0.2*pi*t) + ((0.6*pi*n)^2)*c*sin(0.6*pi*t)];
+
+    elseif strcmpi(shape,'lissajous') == 1
+        rd = [4*c*sin(0.2*pi*t) + rd_0(1);
+              3*c*sin(0.4*pi*t) + rd_0(2)];
+
+        rd_dot = [4*c*(0.2*pi*n)*cos(0.2*pi*t);
+                  3*c*(0.4*pi*n)*cos(0.4*pi*t)];
+    
+        rd_ddot = [-4*c*((0.2*pi*n)^2)*sin(0.4*pi*t);
+                   -3*c*((0.4*pi*n)^2)*sin(0.2*pi*t)];
+
+    elseif strcmpi(shape, "epicycloid") == 1
+        rd = [4*c*cos(0.2*pi*t)-c*cos(0.8*pi*t)-3*c+rd_0(1);
+              4*c*sin(0.2*pi*t)-c*sin(0.8*pi*t)+rd_0(2)];
+
+        rd_dot = [-4*c*(0.2*pi*n)*sin(0.2*pi*t)+c*(0.8*pi*n)*sin(0.8*pi*t);
+                  4*c*(0.2*pi*n)*cos(0.2*pi*t)-c*(0.8*pi*n)*cos(0.8*pi*t)];
+
+        rd_ddot = [-4*c*((0.2*pi*n)^2)*cos(0.2*pi*t)+c*(0.8*pi*n)*cos(0.8*pi*t);
+                   -4*c*((0.2*pi*n)^2)*sin(0.2*pi*t)+c*((0.8*pi*n)^2)*sin(0.8*pi*t)];
+        
     end
         
-        
+    
 end
